07f005ea3ee8b4f73bcf637dae29a0e9d81a7c65
Rafael <rafael_sousa2018@outlook.com>
2021-10-10 20:02:56 -0300

    inclusão do jquery no minify

--- source/minify.php
+++ source/minify.php
@@ -8,6 +8,7 @@ if($_SERVER['SERVER_NAME'] == 'localhost' && $_ENV['MINIFY'] == 'true'){
    $minifierCSS->add(dirname(__DIR__)."/public/assets/css/style.css");
    
    $minifierJS = new Minify\JS();
+   $minifierJS->add(dirname(__DIR__)."/public/assets/vendor/bootstrap/js/jquery-3.6.0.min.js");
    $minifierJS->add(dirname(__DIR__)."/public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js");
    $minifierJS->add(dirname(__DIR__)."/public/assets/js/scripts.js");
 
